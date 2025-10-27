da7598ba56b0d188bd578ec989ae338e2fa27cc5
taha230 <taha.hamedani8@gmail.com>
2025-10-27 16:10:08 +0330

    add gitignore and requirements.txt

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post, Comment
+from django.shortcuts import render
+
 
 def index (request):
     #body
@@ -8,3 +11,14 @@ def index (request):
 def home (request):
     #body
     return HttpResponse('<h3>Welcome to Homepage of blog</h3>')
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {'posts' : posts}
+    return render(request, "posts/post_list.html", context= context)
+
+def post_detail(request, post_id):
+    post = Post.objects.get(pk = post_id)
+    comments = Comment.objects.filter(post = post)
+    context = {'post' : post, 'comments' : comments}
+    return render(request, "posts/post_detail.html", context= context)
--- weblog/urls.py
+++ weblog/urls.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from posts.views import index, home
+from posts.views import index, home, post_list, post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/' , index),
     path('home/', home),
+    path('posts/', post_list , name = "post_list"),
+    path('posts/<int:post_id>/', post_detail, name="post_detail"),
 ]
